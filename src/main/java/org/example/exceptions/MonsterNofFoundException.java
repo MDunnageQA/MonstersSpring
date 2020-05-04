9faeb342244d380e27d396e8ad9687b38f654ec7
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-04 12:28:04 +0100

    created error file for monster not found

---
+++ src/main/java/org/example/exceptions/MonsterNofFoundException.java
@@ -0,0 +1,10 @@
+package org.example.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.EntityNotFoundException;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Monster nonexistant in Database")
+public class MonsterNofFoundException extends EntityNotFoundException {
+}
