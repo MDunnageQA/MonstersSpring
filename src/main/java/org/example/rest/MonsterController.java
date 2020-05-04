5874fae2edcecf80ffe0670d92fdd79de1b908b9
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-04 15:00:02 +0100

    added get monster by id function to the controller

--- src/main/java/org/example/rest/MonsterController.java
+++ src/main/java/org/example/rest/MonsterController.java
@@ -3,10 +3,7 @@ package org.example.rest;
 import org.example.domain.Monsters;
 import org.example.service.MonsterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,9 @@ public class MonsterController {
         return this.service.createMonsters(monsters);
     }
 
+    @GetMapping("/getMonstersByID")
+    public Monsters getMonstersByID(@PathVariable Long id){
+        return this.service.findMonstersByID(id);
+    }
 
 }
