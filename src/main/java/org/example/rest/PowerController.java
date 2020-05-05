2e84e896e027bf6b2c745a98fb6dacf51513f597
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-05 11:24:00 +0100

    created a create method in power controller

--- src/main/java/org/example/rest/PowerController.java
+++ src/main/java/org/example/rest/PowerController.java
@@ -4,6 +4,8 @@ import org.example.domain.Power;
 import org.example.service.PowerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +23,11 @@ public class PowerController {
     @GetMapping("/getAllPowers")
     public List<Power> getAllPowers(){
         return this.service.readPowers();
+    }
+
+    @PostMapping("/createPowers")
+    public Power createPowers(@RequestBody Power power){
+        return this.service.createPowers(power);
     }
 
 }
