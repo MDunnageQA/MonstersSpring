5382be8cd982a0bee06bd4265d5956d5ade5d3da
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-05 10:41:55 +0100

    created power repository

--- src/main/java/org/example/repository/PowerRepository.java
+++ src/main/java/org/example/repository/PowerRepository.java
@@ -1,4 +1,12 @@
 package org.example.repository;
 
-public class PowerRepository {
+import org.example.domain.Power;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PowerRepository extends JpaRepository<Power, Long> {
+
+    Power findByName(String name);
+
 }
