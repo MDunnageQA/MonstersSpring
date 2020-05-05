32ef8d4711e744e897dec4ec1f13fc95b127301b
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-05 11:15:01 +0100

    added readall method to power controller

--- src/main/java/org/example/rest/PowerController.java
+++ src/main/java/org/example/rest/PowerController.java
@@ -1,8 +1,12 @@
 package org.example.rest;
 
+import org.example.domain.Power;
 import org.example.service.PowerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PowerController {
@@ -12,6 +16,11 @@ public class PowerController {
     @Autowired
     public PowerController(PowerService service){
         this.service = service;
+    }
+
+    @GetMapping("/getAllPowers")
+    public List<Power> getAllPowers(){
+        return this.service.readPowers();
     }
 
 }
