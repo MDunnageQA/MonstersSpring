35fc89653ecf52d068c9ec8c2aef16daf428a464
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-06 09:27:34 +0100

    added Monster DTO

---
+++ src/main/java/org/example/dto/MonsterDTO.java
@@ -0,0 +1,51 @@
+package org.example.dto;
+
+public class MonsterDTO {
+
+    private Long id;
+    private String name;
+    private String category;
+    private String description;
+
+    public MonsterDTO(){
+    }
+
+    public MonsterDTO(String name, String category, String description){
+        this.name = name;
+        this.category = category;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
